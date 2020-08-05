1d3ff5958400f25d2c3350132ec14a6ba61377fb
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-08-05 23:53:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let message = []
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return message
+}
+
+let countDown(num){
+  while (num >= 0) {
+  console.log(num);
+    num --;
+  }
+}
